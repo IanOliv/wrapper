56df65b79a60797d35849677b9e1ce6460c07dc0
IanOliv <22185362+IanOliv@users.noreply.github.com>
2024-12-17 10:57:32 -0300

    refactor: remove unused page components and their routes for cleaner codebase

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
 import AddTaskIcon from '@mui/icons-material/AddTask';
-import BugReportIcon from '@mui/icons-material/BugReport';
+// import BugReportIcon from '@mui/icons-material/BugReport';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import HomeIcon from '@mui/icons-material/Home';
-import TerrainIcon from '@mui/icons-material/Terrain';
 
+// import TerrainIcon from '@mui/icons-material/Terrain';
 import asyncComponentLoader from '@/utils/loader';
 
 import { Pages, Routes } from './types';
@@ -17,33 +17,9 @@ const routes: Routes = {
   },
   [Pages.ARfiti]: {
     component: asyncComponentLoader(() => import('@/pages/ARfiti')),
-    path: '/AFiti',
+    path: '/ARfiti',
     title: 'ARfiti',
     icon: GitHubIcon,
-  },
-  [Pages.Page1]: {
-    component: asyncComponentLoader(() => import('@/pages/Page1')),
-    path: '/page-1',
-    title: 'Page 1',
-    icon: AddTaskIcon,
-  },
-  [Pages.Page2]: {
-    component: asyncComponentLoader(() => import('@/pages/Page2')),
-    path: '/page-2',
-    title: 'Page 2',
-    icon: AddTaskIcon,
-  },
-  [Pages.Page3]: {
-    component: asyncComponentLoader(() => import('@/pages/Page3')),
-    path: '/page-3',
-    title: 'Page 3',
-    icon: TerrainIcon,
-  },
-  [Pages.Page4]: {
-    component: asyncComponentLoader(() => import('@/pages/Page4')),
-    path: '/page-4',
-    title: 'Page 4',
-    icon: BugReportIcon,
   },
   [Pages.Card]: {
     component: asyncComponentLoader(() => import('@/pages/Card')),
