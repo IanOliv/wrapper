85a865fe8a0a146e520380fac8c3777af3154e86
IanOliv <22185362+IanOliv@users.noreply.github.com>
2024-12-27 18:14:27 -0300

    feature(component/Base): add base config

---
+++ src/components/Base/index.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import './styles.css';
+
+export function Base() {
+  return (
+    <div className="base-component">
+      <h1>Base</h1>
+    </div>
+  );
+}
